215f9008a843baf1de631ecc02ad546b57f7d384
Jannik Schwarz <jannikschwarz@outlook.com>
2024-07-05 14:36:01 +0200

    responding with ok

--- app/server.go
+++ app/server.go
@@ -16,9 +16,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing to connection", err.Error())
+		os.Exit(1)
+	}
+
 }
